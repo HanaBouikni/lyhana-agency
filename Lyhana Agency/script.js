5a075783136ecbfe374c633c0ae77219319929ae
HanaBouikni <155245299+HanaBouikni@users.noreply.github.com>
2025-07-21 23:00:35 +0200

    first commit

---
+++ Lyhana Agency/script.js
@@ -0,0 +1,10 @@
+  tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        'lyhana-blue': '#0073E6',
+                        'lyhana-light-blue': '#00BFFF'
+                    }
+                }
+            }
+        }
